f66d3d4b4706b55acc052752b33174b2e0563fa5
juanEulogio <eulogiomarti@wisc.edu>
2024-07-08 02:54:14 -0500

    setUp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Container, Row, Col, Button, Image, Card, Carousel } from 'react-bootstrap';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  
+  return ( <div>
+    
+  </div>
+  )
+}
+
+export default App
